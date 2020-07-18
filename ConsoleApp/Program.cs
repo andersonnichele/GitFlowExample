1147debdac8f9defefd8f395d70af06e03e7945f
Anderson L Nichele <nichele.anderson@gmail.com>
2020-07-18 17:14:32 -0300

    Add GetRestOfDivisionOfTwoNumber method

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -11,11 +11,25 @@ namespace ConsoleApp
             var resultOfTwoPlusTwo = SumTwoValues(2, 2);
 
             Console.WriteLine($"2 + 2 is {resultOfTwoPlusTwo}");
+
+            var resultOfFivePerThree = GetRestOfDivisionOfTwoNumber(5, 3);
+
+            Console.WriteLine($"The rest of 5 / 3 is {resultOfFivePerThree}");
+
         }
 
         static int SumTwoValues(int value1, int value2)
         {
             return value1 + ++value2;
+        }
+
+        static int GetRestOfDivisionOfTwoNumber(int dividend, int divisor)
+        {
+            int result;
+
+            Math.DivRem(dividend, divisor, out result);
+
+            return result;
         }
     }
 }
